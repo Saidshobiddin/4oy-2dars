281024714b94120986fef473153584c1135fc21b
unknown <saidshohobiddinmansurov75@gmail.com>
2024-02-23 18:33:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let a = prompt ("Ismiz nima?")
+let b = prompt ("Yoshiz nechida?")
+let c = prompt ("Kaysi maktabta oqiysiz?")
+let d = prompt ("Siz nechinchi sinfda oqiysiz?")
+
+console .log(a,b,c,d)
